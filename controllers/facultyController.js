cf5803a53471e6c4b6196db0bf69a8867f2bc0ae
sabarigbs <sabarigovindarajan@gmail.com>
2018-09-23 19:12:03 +0530

    Added functionalities for respective routes

--- controllers/facultyController.js
+++ controllers/facultyController.js
@@ -1,15 +1,31 @@
 var db = require('../services/dbConnection');
+var statusCodes = require('../constants/httpStatusCodes');
+var response = require('../services/responseFormat');
 
 var facultyController = {
 
     // This function will query the database to display marks of students who are attending their lectures
     getMarksBySubject : function(req,res,next){
       
-        db.query(`SELECT student_id,course_name,marks,remarks FROM marks NATURAL JOIN enrollments NATURAL JOIN courses WHERE teaches_id = (SELECT teaches_id FROM teaches WHERE faculty_id = ? AND course_id = ? ) AND occurrence_id = ? `,[req.params.facultyId,req.query.course_id,req.params.occurrenceId],function(err,data,fields){
+        db.query(`SELECT 
+        student_id,course_name,marks,remarks 
+        FROM 
+        marks NATURAL JOIN enrollments 
+        NATURAL JOIN courses 
+        WHERE teaches_id = (SELECT teaches_id FROM teaches WHERE faculty_id = ? AND course_id = ? ) AND occurrence_id = ? `,[req.params.facultyId,req.query.course_id,req.params.occurrenceId],function(err,data,fields){
             if(err)
-                throw err;
-            message={data};
-            res.json(message);
+                response.sendErrorResponse(res,statusCodes.INTERNAL_SERVER_ERROR);
+            else if(data.length !== 0){
+                    message={
+                    'success':true,
+                    data
+                };
+                response.sendSuccessResponse(res,statusCodes.OK,message);
+            }
+            else{
+                response.sendErrorResponse(res,statusCodes.NOT_FOUND);
+            }
+            
         });
 
     },
@@ -17,11 +33,32 @@ var facultyController = {
     // This function updates mark of a subject depending on his remarks and proof
     updateMarks : function(req,res,next){
 
+        db.query(`UPDATE marks SET marks = ? WHERE mark_id = ?`,[req.body.marks.req.params.markId],function(err,data,fields){
+            if(err)
+                response.sendErrorResponse(res,statusCodes.INTERNAL_SERVER_ERROR);
+            else if(data.affectedRows === 1){
+                message={
+                    'success':true
+                };
+            }
+            else
+                response.sendErrorResponse(res,statusCodes.NOT_FOUND);
+        });
+
     },
 
     // This function will insert attendance details for a particular lecture 
     insertSubjectwiseAttendance : function(req,res,next){
-        db.query('',[],function(err,data,fields){
+        
+        db.query(`INSERT INTO student_attendance(student_id,period,course_id,date) VALUES ? `,[valArr],function(err,data,fields){
+            if(err)
+                response.sendErrorResponse(res,statusCodes.INTERNAL_SERVER_ERROR);
+            else{
+                message = {
+                    'success':true,
+                };
+                response.sendSuccessResponse(res,statusCodes.RECORD_CREATED);
+            }
 
         });
     }, 
@@ -60,10 +97,6 @@ var facultyController = {
 
         });
     },
-
-    dummyFunction : function(req,res,next){
-        
-    }
 };
 
 module.exports = facultyController;
--- controllers/studentController.js
+++ controllers/studentController.js
@@ -9,11 +9,12 @@ var studentController = {
         
         var message;
 
-        db.query('SELECT student,student_name,course_id,course_name,mark_id,marks FROM students NATURAL JOIN enrollments NATURAL JOIN courses NATURAL JOIN marks WHERE student_id = ? AND occurrence_id = ?',[req.params.studentId,req.params.occurrenceId],function(err,data,fields){
+        db.query('SELECT student_id,student_name,course_id,course_name,mark_id,marks FROM students NATURAL JOIN enrollments NATURAL JOIN courses NATURAL JOIN marks WHERE student_id = ? AND occurrence_id = ?',[req.params.studentId,req.params.occurrenceId],function(err,data,fields){
             
-            if(err)
+            if(err){
+               console.log(err);
                response.sendErrorResponse(res,statusCodes.INTERNAL_SERVER_ERROR);
-            else if(data.length !== 0 && data[0] !== undefined){
+            }else if(data.length !== 0 && data[0] !== undefined){
             
                 message={
                     'success':true,
@@ -33,7 +34,7 @@ var studentController = {
     // This function updates remarks for a mark
     updateRemarks : function(req,res,next){
         
-        db.query(`UPDATE marks SET remarks = 'verified' WHERE mark_id= ?`,[req.params.markId],function(err,data,fields){
+        db.query(`UPDATE marks SET remarks = ? WHERE mark_id= ? WHERE onduty_id = ? `,[req.body.remarks,req.params.markId],function(err,data,fields){
             if(err)
                response.sendErrorResponse(res,statusCodes.INTERNAL_SERVER_ERROR);
             else if(data.affectedRows === 1){
@@ -43,7 +44,7 @@ var studentController = {
             }
             else
                 response.sendErrorResponse(res,statusCodes.NOT_FOUND);
-            res.json(message);
+            
         });
         
     },
@@ -77,13 +78,28 @@ var studentController = {
     // This function retrieves the onduty requests applied by a student
     viewAppliedOnDuty : function(req,res,next){
 
-        db.query(`SELECT * FROM on_duty WHERE student_id = ? `,[req.params.studentId],function(err,data,fields){
+        db.query(`SELECT 
+            date,
+            max(case when period = 1 then onduty_id end) period1,
+            max(case when period = 2 then onduty_id end) period2,
+            max(case when period = 3 then onduty_id end) period3,
+            max(case when period = 4 then onduty_id end) period4,
+            max(case when period = 5 then onduty_id end) period5,
+            max(case when period = 6 then onduty_id end) period6,
+            max(case when period = 7 then onduty_id end) period7 
+            FROM on_duty INNER JOIN students ON on_duty.student_id = students.student_id WHERE on_duty.student_id = ?
+            GROUP BY date `,[req.params.studentId],function(err,data,fields){
             if(err)
                response.sendErrorResponse(res,statusCodes.INTERNAL_SERVER_ERROR);
-            message={
-                'success':true
-            };
-            res.json(message);
+            else if(data.length !== 0){
+                    message={
+                    'success':true,
+                    data
+                };
+                response.sendSuccessResponse(res,statusCodes.OK,message);
+            }
+            else
+                response.sendErrorResponse(res,statusCodes.NOT_FOUND);
         })
     },
 
@@ -91,11 +107,41 @@ var studentController = {
     // This function updates the applied onduty information of a student
     updateAppliedOnDuty : function(req,res,next){
 
+        db.query(`UPDATE on_duty SET period = ? reason = ? date = ?`,[],function(err,data,fields){
+            if(err)
+                response.sendErrorResponse(res,statusCodes.INTERNAL_SERVER_ERROR);
+            else if(data.affectedRows === 1){
+                message = {
+                    'success':true,
+                    'message':'Onduty request updated successfully!'
+                }
+                response.sendSuccessResponse(res,statusCodes.OK,message);
+            }
+            else{
+                response.sendErrorResponse(res,statusCodes.NOT_FOUND);
+            }
+            
+        });
+
     },
 
     // This function deletes the applied onduty of student
     deleteOnDutyRequest : function(req,res,next){
-
+        db.query(`DELETE FROM on_duty WHERE onduty_id = ? `,[req.params.ondutyId],function(err,data,fields){
+            if(err)
+                response.sendErrorResponse(res,statusCodes.INTERNAL_SERVER_ERROR);
+            else if(data.affectedRows === 1){
+                message = {
+                    'success':true,
+                    'message':'Onduty request deleted successfully!'
+                }
+                response.sendSuccessResponse(res,statusCodes.OK,message);
+            }
+            else{
+                response.sendErrorResponse(res,statusCodes.NOT_FOUND);
+            }
+            
+        });
     }
 
 
