0c4dc1b30cb08ae6bbc278313c057f43992bda68
sabarigbs <sabarigovindarajan@gmail.com>
2018-10-19 11:23:12 +0530

    Added authentication middleware for checking whether the user is authenticated

---
+++ middlewares/checkAuthenticated.js
@@ -0,0 +1,10 @@
+var response = require('../services/responseFormat');
+var statusCodes = require('../constants/httpStatusCodes');
+
+module.exports = {
+    isAuthenticated : function(req,res,next){
+        if(req.isAuthenticated())
+            next()
+        response.sendErrorResponse(res,statusCodes.UNAUTHORIZED);
+    }
+}
