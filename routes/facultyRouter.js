d7e762a57699d9816d00469f9410bfb1c72366c9
sabarigbs <sabarigovindarajan@gmail.com>
2018-11-05 21:27:18 +0530

    Protected routes for faculty

--- routes/facultyRouter.js
+++ routes/facultyRouter.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var facultyController = require('../controllers/facultyController');
+var authenticationMiddleware = require('../middlewares/checkAuthenticated');
+
+//Routes that deal with marks 
+router.get('/:facultyId/marks/:occurrenceId',authenticationMiddleware.isAuthenticated,facultyController.getMarksBySubject);
+router.post('/:facultyId/marks/:occurrenceId',authenticationMiddleware.isAuthenticated,facultyController.insertMarksBySubject);
+
+//Routes that deal with attendance
+router.post('/:facultId/attendance',authenticationMiddleware.isAuthenticated,facultyController.insertSubjectwiseAttendance);
 
 
-router.get('/:facultyId/marks/:occurrenceId',facultyController.getMarksBySubject);
-router.post('/:facultyId/marks/:occurrenceId',facultyController.insertMarksBySubject);
-
-
-router.post('/:facultId/attendance',facultyController.insertSubjectwiseAttendance);
+//Routes that deal with onduty
 
 
 module.exports = router;
