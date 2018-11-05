62a076788c9bbb6966adc40db3390979861f5d1e
sabarigbs <sabarigovindarajan@gmail.com>
2018-11-05 21:21:36 +0530

    Configuration added in seperate files for db and passport

---
+++ config/dbConfig.js
@@ -0,0 +1,17 @@
+module.exports = {
+    
+    LOCAL : {
+        HOST : 'localhost',
+        USER : 'root',
+        PASSWORD : '',
+        DATABASE : 'student_connect'
+    },
+
+    CLOUD : {
+        HOST : 'db4free.net',
+        USER : 'sabari',
+        PASSWORD : 'sabari.b',
+        DATABASE : 'student_connect'
+    }
+
+}
---
+++ config/passport.js
@@ -0,0 +1,68 @@
+var LocalStrategy = require('passport-local').Strategy;
+var db = require('../services/dbConnection');
+
+module.exports = function(passport) {
+  passport.serializeUser(function(user, done) {
+    console.log("id", user);
+    if(user['role'] === 'student'){
+      keyToUse = 'student_id';
+    }else{
+      keyToUse = 'faculty_id';
+    }
+    done(null, {
+      id:user[keyToUse],
+      role: user['role']
+    });
+  });
+
+  passport.deserializeUser(function(user, done) {
+    console.log("deserialize", user);
+    var tableName;
+    var columnName;
+    if(user['role'] === 'student'){
+      tableName = 'students';
+      columnName = ['student_id'];
+    }else{
+      tableName = 'faculty';
+      columnName = ['faculty_id'];
+    }
+    db.query("SELECT * FROM ?? WHERE ?? = ?", [tableName,columnName,user['id']], function(
+      err,
+      data,
+      fields
+    ) {
+      done(err, data[0]);
+    });
+  });
+
+  passport.use(new LocalStrategy({passReqToCallback:true},
+    function(req,username,password,done){
+      var tableName;
+      var columnName;
+      if(req.body.role == 'student'){
+        tableName = 'students';
+        columnName = ['student_id'];
+      }else{
+        tableName = 'faculty';
+        columnName = ['faculty_id'];
+      }
+
+      console.log(username,password,req.body.role);
+      db.query("SELECT * FROM ?? WHERE ?? = ?",[tableName,columnName,username],function(err,data,fields){
+        console.log("Came here");
+        if(err){
+          return done(err);
+        }
+        if(data.length === 0){
+          return done(null,false);
+        }
+        if(data[0]['password'] !== password){
+          return done(null,false);
+        }
+        data[0]['role']=req.body.role;
+        return done(null,data[0]);
+      })
+    }
+  ));
+  
+};
