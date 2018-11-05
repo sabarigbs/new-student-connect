e424b9ea6a7f615a0911c16dff85977b68a56017
sabarigbs <sabarigovindarajan@gmail.com>
2018-11-05 21:28:13 +0530

    Restructured for moving out passport js config to seperate files

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,58 +1,29 @@
-var express = require('express');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+var express = require("express");
 var router = express.Router();
-var db = require('../services/dbConnection');
-var response = require('../services/responseFormat');
-var statusCodes = require('../constants/httpStatusCodes');
+var response = require("../services/responseFormat");
+var statusCodes = require("../constants/httpStatusCodes");
 
+module.exports = function(passport) {
 
-passport.use(new LocalStrategy({passReqToCallback:true},
-  function(req,username,password,done){
-    console.log(req.body.role);
-    db.query("SELECT * FROM students WHERE student_id = ?",[username],function(err,data,fields){
-      if(err){
-        return done(err);
-      }
-      if(data.length === 0){
-        return done(null,false);
-      }
-      if(data[0]['password'] !== password){
-        return done(null,false);
-      }
-      return done(null,data[0]['student_id']);
-    })
-  }
-));
+  router.post("/signup", function(req, res, next) {});
 
-passport.serializeUser(function(id,done){
-  done(null,id);
-});
-
-passport.deserializeUser(function(id,done){
-  db.query("SELECT * FROM students WHERE student_id = ?",[id],function(err,data,fields){
-    done(err,data[0]);
-  });
-});
-
-router.post('/signup', function(req, res, next) {
-  
-});
-
-router.post('/login',passport.authenticate('local'), function(req, res, next) {
-    console.log(req.isAuthenticated());
+  router.post("/login", passport.authenticate("local"), function(
+    req,
+    res,
+    next
+  ) {
     message = {
-      'success':true
+      success: true
     };
-    response.sendSuccessResponse(res,statusCodes.OK,message);
-});
+    response.sendSuccessResponse(res, statusCodes.OK, message);
+  });
 
-router.get('/logout', function(req, res, next) {
-  req.logout();
-  message = {
-    'success':true
-  };
-  response.sendSuccessResponse(res,statusCodes.OK,message);
-});
-
-module.exports = router;
+  router.get("/logout", function(req, res, next) {
+    req.logout();
+    message = {
+      success: true
+    };
+    response.sendSuccessResponse(res, statusCodes.OK, message);
+  });
+  return router;
+};
